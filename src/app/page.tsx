db3ff13b425053dcec1cf611e39d8acff3a4ab3b
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2024-03-23 13:59:51 -0700

    created product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import { Button, buttonVariants } from '@/components/ui/button';
-import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react';
-import Image from 'next/image'
-import Link from 'next/link';
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import ProductReel from "../components/ProductReel";
+import { Button, buttonVariants } from "../components/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const perks = [
   {
@@ -25,7 +26,6 @@ const perks = [
   },
 ];
 
-
 export default function Home() {
   return (
     <>
@@ -47,25 +47,38 @@ export default function Home() {
             <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
-        {/* TODO: List products */}
+        <ProductReel
+          query={{ sort: "desc", limit: 4 }}
+          title="brand new"
+          href="/products"
+        />
       </MaxWidthWrapper>
-      <section className='border-t border-gray-200 bg-gray-50'>
-        <MaxWidthWrapper className='py-20'>
-          <div className='grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0'>
-            {perks.map((perk) => (<div key={perk.name} className='text-center md:flex md:items-start md:text-left lg:block lg:text-center'>
-              <div className='md:flex-shrink-0 flex justify-center'>
-                <div className='h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900'>
-                  {<perk.Icon className='w-1/3 h-1/3'/>}  
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className="text-center md:flex md:items-start md:text-left lg:block lg:text-center"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    {<perk.Icon className="w-1/3 h-1/3" />}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perk.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
                 </div>
               </div>
-              <div className='mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6'>
-                <h3 className='text-base font-medium text-gray-900'>{perk.name}</h3>
-                <p className='mt-3 text-sm text-muted-foreground'>{perk.description}</p>
-              </div>
-            </div>))}
+            ))}
           </div>
         </MaxWidthWrapper>
-    </section>
+      </section>
     </>
   );
 }
